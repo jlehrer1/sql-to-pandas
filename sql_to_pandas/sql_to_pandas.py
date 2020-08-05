7f0e0963de5c265212a842957206e5d07819427f
Julian Lehrer <jmlehrer@ucsc.edu>
2020-08-05 14:11:22 -0700

    Initial commit

--- sql_to_pandas/sql_to_pandas.py
+++ sql_to_pandas/sql_to_pandas.py
@@ -1,1 +1,19 @@
 """Main module."""
+
+import pandas as pd
+import sqlite3 
+
+class SQLtoPD:
+    def __init__(self, strict=True):
+        self.strict = strict
+
+    def parse(df: pd.DataFrame, string: str) -> pd.DataFrame:
+        """Parses the SQL string into pandas and returns its results onto the DataFrame"""
+        
+        _is_valid_sql(string)
+
+
+    def _is_valid_sql(string):
+        """Checks if the given SQL query is valid SQL."""
+
+        pass
