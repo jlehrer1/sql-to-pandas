ef200e8626f67772c839bf6ede4fbad82417f698
Julian Lehrer <jmlehrer@ucsc.edu>
2020-08-05 22:28:45 -0700

    Started parse_rows, added method for checking if word is a sql keyword

--- sql_to_pandas/sql_to_pandas.py
+++ sql_to_pandas/sql_to_pandas.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import sqlite3
-import sys, os
+import re 
 
 from .keywords import _get_sql_keywords
 
@@ -10,17 +10,19 @@ class SQLtoPD:
     def __init__(self, strict=True):
         self.strict = strict
     
-    def _is_valid_sql(self, df: pd.DataFrame, string: str):
-        """Checks if the given SQL query is valid SQL syntactically."""
+    def _issqlkeyword(self, string: str) -> bool:
         # Get list of SQL reserved keywords
         keywords = _get_sql_keywords()
+        return string in keywords
 
+    def _is_valid_sql(self, df: pd.DataFrame, string: str):
+        """Checks if the given SQL query is valid SQL syntactically."""
         # Split the string so each word can be parsed
         string = string.split()
 
         # Extract the not sql keywords (computationally expensive!)
-        not_sql_words = [word for word in string if word.upper() not in keywords]
-        
+        not_sql_words = [word for word in string if not self._issqlkeyword(word.upper())]
+
         # Then use set differencing to get the keywords
         sql_words = list(set(string).difference(set(not_sql_words)))
         sql_words = [word.lower() for word in sql_words]
@@ -37,25 +39,41 @@ class SQLtoPD:
                 raise ValueError('Error: {} column not found'.format(possible_col))
         
     def _parse_columns(self, df: pd.DataFrame, string: str) -> pd.DataFrame:
+        """Parses which columns to use from the DataFrame. Runs in place of SELECT <cols> FROM <df>"""
         if string[0] == 'select':
             if string[1] == '*':
                 pass 
             else:
                 cols = []
                 i = 1
+
                 # Iterate through all columns being requested
-                while string[i] != 'from':
+                while string[i] != '\n':
                     cols.append(string[i])
                     i += 1
 
-                # Clean commas from columns
+                # Clean commas from column list styling in SQL
                 for idx, item in enumerate(cols):
                     if item[-1] == ',':
                         cols[idx] = item[:-1]
+                
                 df = df[cols]
         return df
 
     def _parse_rows(self, df: pd.DataFrame, string: str) -> pd.DataFrame:
+        """Parses which rows to use from the DataFrame. Runs in place of WHERE <condition>"""
+        # If there is no row select condition return out and continue
+        if 'where' not in string:
+            return df
+
+        start_parse_location = string.index('where') + 1
+        i = start_parse_location
+
+        # Make sure we're still parsing
+        conditions = []
+        while string[i] != '\n' or i != len(string) - 1:
+            conditions.append(i)
+
         return df
 
     def parse(self, df: pd.DataFrame, string: str) -> pd.DataFrame:
@@ -63,7 +81,7 @@ class SQLtoPD:
         self._is_valid_sql(df=df, string=string)
 
         # Turn the string to lowercase and split into an array for processing
-        string_split = string.lower().split()
+        string_split = re.findall(r'\S+|\n', string.lower())
         
         # Parse columns
         df = self._parse_columns(df, string_split)
